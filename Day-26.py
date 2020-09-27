9d282ebe26cad6feb24fbd4a43f950fd509f8f43
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-09-27 11:04:56 +0530

    add Day-26.py

---
+++ Day-26.py
@@ -0,0 +1,20 @@
+def calculate_fine(expected, actual):
+	if actual[2] < expected[2]:
+		return 0
+	elif actual[2] == expected[2]:
+		if actual[1] < expected[1]:
+			return 0
+		elif actual[1] == expected[1]:
+			if actual[0] <= expected[0]:
+				return 0
+			else:
+				return 15 * (actual[0] - expected[0])
+		else:
+			return 500 * (actual[1] - expected[1])
+	else:
+		return 10000
+
+actual = list(map(int, input().split()))
+expected = list(map(int, input().split()))
+
+print(calculate_fine(expected, actual))
